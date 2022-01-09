7b886a02055ec46c058cb8e2d8e4dc6585a88c7d
denikryt <danil.mirzoev@gmail.com>
2022-01-10 02:49:18 +0300

    game fixed

--- y.version/learn.py
+++ y.version/learn.py
@@ -55,13 +55,23 @@ class Learn():
             self.send_message(message, call, case='delete', message_id=self.game_window)
             self.start(message, call)
 
-        if call.data == 'prompt':
-            if self.prompt > 0:
-                self.prompt -= 1
-                self.spelling += self.chars[self.char]
+        if call.data == 'help':
+            if self.chars[self.char] == ' ':
                 self.char += 1
 
-                self.send_message(message, call, case='prompt')
+            if self.help > 0:
+                self.help -= 1
+
+                self.stars[self.char] = self.chars[self.char]
+                self.spelling = ''.join(self.stars)
+
+                if self.spelling == self.random_word.lower():
+                    self.send_message(message, call, case='loose')
+                    return
+
+                self.char += 1
+
+                self.send_message(message, call, case='help')
             else:
                 return
             # return
@@ -143,109 +153,7 @@ class Learn():
         return user_name, user_id
 
     def random_words(self, message, call):
-
-        if message:
-            user_name = message.from_user.first_name
-            user_id = message.chat.id
-        if call :
-            user_name = call.from_user.first_name
-            user_id = call.from_user.id
-
-        if len(self.temp_choise_list) == 0:
-            # markup = types.InlineKeyboardMarkup(row_width=2)
-            # item1 = types.InlineKeyboardButton('Повторить по новой', callback_data='repeat')
-            # markup.add(item1)
-            # bot.send_message(user_id,'Слов для повтора нету!\n', reply_markup=markup)
-            return
-        else:
-            self.random_word = random.choice(self.temp_choise_list)
-
-        which_rand = [self.random_word, self.vocab[self.random_word]]
-        self.rand_choice = random.choice(which_rand)
-        list_for_answers = self.words.copy()
-
-        if type(self.rand_choice) == str:
-            self.temp_choise_list.remove(self.random_word)
-            list_for_answers.remove(self.random_word)
-
-            r_trans = random.sample(list_for_answers, 2)
-            self.rand_tran1 = random.choice(self.vocab[self.random_word])
-            rand_tran2 = random.choice(self.vocab[r_trans[0]])
-            rand_tran3 = random.choice(self.vocab[r_trans[1]])
-            # rand_tran4 = random.choice(self.vocab[r_trans[2]])
-            # rand_tran5 = random.choice(self.vocab[r_trans[3]])
-            # rand_tran6 = random.choice(self.vocab[r_trans[4]])
-
-        if type(self.rand_choice) == list:
-            self.temp_choise_list.remove(self.random_word)
-            list_for_answers.remove(self.random_word)
-
-            r_trans = random.sample(list_for_answers, 2)
-            self.rand_tran1 = self.random_word
-            rand_tran2 = r_trans[0]
-            rand_tran3 = r_trans[1]
-            # rand_tran4 = r_trans[2]
-            # rand_tran5 = r_trans[3]
-            # rand_tran6 = r_trans[4]
-
-
-        item1 = types.InlineKeyboardButton(self.rand_tran1, callback_data='True')
-        item2 = types.InlineKeyboardButton(rand_tran2, callback_data='False')
-        item3 = types.InlineKeyboardButton(rand_tran3, callback_data='False')
-        # item4 = types.InlineKeyboardButton(rand_tran4, callback_data='False')
-        # item5 = types.InlineKeyboardButton(rand_tran5, callback_data='False')
-        # item6 = types.InlineKeyboardButton(rand_tran6, callback_data='False')
-
-        self.rand_answers = [item1, item2, item3]#, item4, item5]
-        random.shuffle(self.rand_answers)
-
-        markup = types.InlineKeyboardMarkup(row_width=1)
-        markup.add(*self.rand_answers)
-
-        file_name = 'users.yaml'
-
-        # with open(directory(file_name), 'r', encoding='utf-8') as f:
-        #     try:
-        #         result = yaml.load(f, Loader=yaml.FullLoader)['users'][user_name]
-        #     except TypeError:
-        #         pass
-
-        try:
-            last_notify = open_yaml()['users'][user_name]['last notify']
-            bot.delete_message(user_id, message_id=last_notify)
-        except KeyError:
-            pass
-        except apihelper.ApiTelegramException:
-            print('НЕ УДАЛИЛОСЬ!')
-
-
-        bot.send_message(user_id, 'Переведи слово:\n' + str(self.rand_choice), reply_markup=markup)
-        # write(user_name, user_id, target='last message')
-        write(user_name, user_id, target='last notify')
-
-
-        # if not message == None:
-        #     self.test_window = message_ID+1
-        #     self.result_window = message_ID+2
-        #     bot.send_message(user_ID, 'Выбери вариант')
-        #     bot.send_message(user_ID, 'Выбери вариант')
-
-        # if not call == None:
-        #     if call.data == 'learn': # or call.data == 'repeat'
-        #         self.test_window = message_ID+1
-        #         self.result_window = message_ID+2
-        #         bot.send_message(user_ID, 'Переведи слово:\n' + str(self.rand_choice), reply_markup=markup)
-        #         bot.send_message(user_ID, 'Выбери вариант')
-
-        #     elif call.data == 'repeat':
-        #         bot.edit_message_text(chat_id=user_ID, message_id=message_ID, text='Переведи слово:\n' + str(self.rand_choice), reply_markup=markup)
-        #         bot.send_message(user_ID, 'Выбери вариант')
-
-        #     else:
-        #         bot.edit_message_text(chat_id=user_ID, message_id=message_ID+1, text='Переведи слово:\n' + str(self.rand_choice), reply_markup=markup)
-        #         bot.send_message(user_ID, 'Выбери вариант')
-        #         # bot.send_message(user_ID, 'Переведи слово:\n' + str(self.rand_choice), reply_markup=markup)
-        #         # bot.send_message(user_ID, 'Выбери вариант')
+        pass
 
     def start(self, message, call):
         user_name, user_id = self.name_id(message, call)
@@ -253,15 +161,20 @@ class Learn():
             self.last_message = message.message_id
 
         self.random_word = random.choice(self.temp_choise_list)
+        # self.random_word = 'dramatic improvement'
         self.translate = ','.join(self.vocab[self.random_word])
 
         self.loose = False
         self.attempts = 3
-        self.prompt = 3
+        self.help = 3
+        self.char = 0
         self.spelling = ''
         self.chars = []
         self.chars = [char.lower() for char in self.random_word]
-        self.char = 0
+        self.stars = []
+        self.stars = ['*' if not x == ' ' else ' ' for x in self.random_word]
+
+        self.spelling = ''.join(self.stars)
 
         self.send_message(message, call, case='send')
 
@@ -272,6 +185,10 @@ class Learn():
         word = self.random_word.lower()
         char = self.chars[self.char]
 
+        if char == ' ':
+            self.char += 1
+            char = self.chars[self.char]
+
         self.send_message(message, call, case='delete', message_id=self.last_message)
 
         if self.attempts > 0:
@@ -281,7 +198,8 @@ class Learn():
                 return
 
             if text == char:
-                self.spelling += char
+                self.stars[self.char] = char
+                self.spelling = ''.join(self.stars)
                 self.char += 1
 
                 if self.spelling == word:
@@ -305,13 +223,13 @@ class Learn():
 
     def send_message(self, message=None, call=None, case=None, message_id=None):
         user_name, user_id = self.name_id(message, call)
-        cases = ['correct', 'incorrect', 'prompt']
+        cases = ['correct', 'incorrect', 'help']
 
-        text = f'Переведи слово\n<b>{self.translate}</b>\n{self.spelling}\nПопыток: {str(self.attempts)}\nПодсказок: {str(self.prompt)}'
+        text = f'Переведи слово\n<b>{self.translate}</b>\n{self.spelling}\nПопыток: {str(self.attempts)}\nПодсказок: {str(self.help)}'
 
         markup = types.InlineKeyboardMarkup(row_width=2)
 
-        item1 = types.InlineKeyboardButton('Подсказка', callback_data='prompt')
+        item1 = types.InlineKeyboardButton('Подсказка', callback_data='help')
         item2 = types.InlineKeyboardButton('Сдаюсь', callback_data='give_up')
         item3 = types.InlineKeyboardButton('Новое слово', callback_data='new')
 
